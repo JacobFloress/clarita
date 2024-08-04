2fac3e728d17c24e6f5140ebe0176c9cb5195ce8
Jacob Flores <140263607+JacobFloress@users.noreply.github.com>
2024-08-04 11:09:42 -0500

    java.js

--- java.js
+++ java.js
@@ -2,9 +2,9 @@ const btnInicio = document.querySelector('#btnInicial');
 const message = document.querySelector('#message');
 
 const messages = [
-  "De todas las vidas, de todas las lineas de tiempo, todos los millones, trillones, sixtillones, veite-mil-millones de galaxias, nosotros cruzamos caminos...Tu y yo. Estamos compartiendo esta vida. ¿Cuantas son las posibilidades?",
-  "Que si vales la pena? Creo yo que con 'valer' la pena te quedas corta mi amor. Tú vales cada esfuerzo, cada intento, cada lucha, cada obstáculo. Tú vales toda la espera. Vales más de lo que yo soy capaz de expresar",
-  "Eres tú, has sido tú en los últimos meses desde que llegaste a mi vida, eres tu a las 2 de la mañana o a las 6 de la tarde, eres tú cuando despierto, eres tú cuando duermo. Eres tú."
+  "De todas las vidas, de todas las lineas de tiempo, todos los millones, trillones, sixtillones, veite-mil-millones de galaxias, nosotros cruzamos caminos...Tu y yo. Estamos compartiendo esta vida. ¿Cuantas son las posibilidades?. Disfruta de tu cumpleaños mi vida! Te Quiero <3",
+  "Que si vales la pena? Creo yo que con 'valer' la pena te quedas corta mi amor. Tú vales cada esfuerzo, cada intento, cada lucha, cada obstáculo. Tú vales toda la espera. Vales más de lo que yo soy capaz de expresar. Te deseo lo mejor princesa en este dia tan especial para mi y para ti! no pares de sonreir :) Me encantas :3",
+  "Eres tú, has sido tú en los últimos meses desde que llegaste a mi vida, eres tu a las 2 de la mañana o a las 6 de la tarde, eres tú cuando despierto, eres tú cuando duermo. Eres tú... Ya son 23 vueltas mi amor!!! Disfruta mucho de tu dia! Te Amo ♡"
 ]
 
 btnInicio.addEventListener('click', function() {
